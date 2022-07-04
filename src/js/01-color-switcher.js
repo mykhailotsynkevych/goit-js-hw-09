1a3ac5c95602a00ea75229a70bce742c49cc718a
Mykhailo Tsynkevych <99801093+mykhailotsynkevych@users.noreply.github.com>
2022-07-04 20:17:59 +0300

    01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+const colorShow = {
+  timerId: null,
+  isActive: false,
+
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    this.timerId = setInterval(() => {
+      this.isActive = true;
+
+      document.body.style.backgroundColor = getRandomHexColor();
+    //   console.log('))');
+    }, 1000);
+  },
+
+  stop() {
+    clearInterval(this.timerId);
+    this.isActive = false;
+  },
+};
+
+startBtn.addEventListener('click', () => {
+  colorShow.start();
+});
+
+stopBtn.addEventListener('click', () => {
+  colorShow.stop();
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
