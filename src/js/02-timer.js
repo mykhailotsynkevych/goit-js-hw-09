a3555c9e95555ba2f5759a3a0b80c228ae5cd72e
Mykhailo Tsynkevych <99801093+mykhailotsynkevych@users.noreply.github.com>
2022-07-05 23:45:18 +0300

    02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,20 +1,33 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const startBtn = document.querySelector('button[data-start]');
+// console.log(startBtn);
+startBtn.disabled = true;
 
 const date = new Date();
+const nowTime = date.getTime();
+// console.log(nowTime);
 
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    nowDate: date,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
+  isActive: false,
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (nowTime > selectedDates[0].getTime()) {
+        startBtn.disabled = true;
+        window.alert('Please choose a date in the future');
+    } else {
+    console.log(selectedDates[0]);
+      startBtn.disabled = false;
+      }
 
-        console.log(selectedDates[0]);
-    },
+  },
 };
 
-flatpickr("#datetime-picker", options);
 
-console.log(date);
+flatpickr('#datetime-picker', options);
+
+// console.log(date);
