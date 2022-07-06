7e29ddf416cc1c25b92ae55389ad185b6d224edb
Mykhailo Tsynkevych <99801093+mykhailotsynkevych@users.noreply.github.com>
2022-07-06 22:36:49 +0300

    ((((

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,37 +2,23 @@ const delay = document.querySelector('input[name="delay"]');
 const step = document.querySelector('input[name="step"]');
 const amount = document.querySelector('input[name="amount"]');
 const submit = document.querySelector('button[type="submit"]');
+const form = document.querySelector('.form')
+
+
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    return 12
-  } else {
-    return 14
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
 
-  
+  if (shouldResolve) {
+    resolve({position, delay});
+  } else {
+     reject({position, delay});
+  }
+  })
+
 }
 
-
-// console.log(submit);
-
-delay.addEventListener('input', (e) => {
-const delayNumber = e.target.value;
-console.log(delayNumber);
-})
-
-
-step.addEventListener('input', (e) => {
-  console.log(e.target.value);
-})
-
-amount.addEventListener('input', (e) => {
-  console.log(e.target.value);
-})
-
-submit.addEventListener('click', (e) => {
-  e.preventDefault();
 
 createPromise(2, 1500)
   .then(({ position, delay }) => {
@@ -42,7 +28,29 @@ createPromise(2, 1500)
     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
   });
 
-  console.log(234);
+
+
+// console.log(submit);
+
+delay.addEventListener('input', (e) => {
+  return e.target.value;
+
 })
+
+// console.log(one);
+step.addEventListener('input', (e) => {
+  console.log(e.target.value);
+})
+
+amount.addEventListener('input', (e) => {
+  console.log(e.target.value);
+})
+
+submit.addEventListener('click', (e) => {
+  e.preventDefault()
+});
+
+
+
 
 
