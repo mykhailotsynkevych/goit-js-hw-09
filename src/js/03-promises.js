5d634e3d38cb1cffdcf35b486f7e67d5f0bb6b04
Mykhailo Tsynkevych <99801093+mykhailotsynkevych@users.noreply.github.com>
2022-07-08 19:31:08 +0300

    finall

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,56 +1,57 @@
-const delay = document.querySelector('input[name="delay"]');
-const step = document.querySelector('input[name="step"]');
-const amount = document.querySelector('input[name="amount"]');
-const submit = document.querySelector('button[type="submit"]');
-const form = document.querySelector('.form')
+import Notiflix from 'notiflix';
+
+const refs = {
+  form: document.querySelector('.form'),
+}
+
+refs.form.addEventListener('submit', onSearch);
+
+let array = [];
+let delay = null;
+let step = null;
+let position = 0;
 
 
+function onSearch(e) {
+  e.preventDefault();
+
+  const input = e.currentTarget.elements;
+  delay = Number(input.delay.value);
+  step = Number(input.step.value);
+
+  const inputValue = Number(input.amount.value);
+
+  for (let i = 1; i <= inputValue; i += 1) {
+  array.push(i)
+  }
+  
+  array.map(number => {
+    position = number;
+    if (position !== 1) {
+      delay += step;
+    }  createPromise(position, delay)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  })
+  .finally(() => {
+    refs.form.reset();
+  });
+  })
+}
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
+      const shouldResolve = Math.random() > 0.3;
+  setTimeout(() => {
+    if (shouldResolve) {
+      resolve({position, delay});
+    } else {
+      reject({position, delay});
+    }
+  }, delay);
 
-  if (shouldResolve) {
-    resolve({position, delay});
-  } else {
-     reject({position, delay});
-  }
-  })
-
-}
-
-
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
-
-
-
-// console.log(submit);
-
-delay.addEventListener('input', (e) => {
-  return e.target.value;
-
-})
-
-// console.log(one);
-step.addEventListener('input', (e) => {
-  console.log(e.target.value);
-})
-
-amount.addEventListener('input', (e) => {
-  console.log(e.target.value);
-})
-
-submit.addEventListener('click', (e) => {
-  e.preventDefault()
 });
-
-
-
-
-
+}
